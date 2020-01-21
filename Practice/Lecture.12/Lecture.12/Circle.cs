687c0abc45c2a78c6443c14a4aa6b93f391f62f5
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-21 19:32:26 +0300

    Lecture 12 is done

---
+++ Practice/Lecture.12/Lecture.12/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_11
+{
+    class Circle : Shapes
+    {
+        private readonly double _radius;
+        public Circle(double radius)
+        {
+            if (radius <= 0)
+            {
+
+                this._radius = 1;
+            }
+            else if (radius > 10)
+            {
+                this._radius = 10;
+            }
+            else
+                this._radius = radius;
+        }
+        public override double FigureSquare() => Math.PI * _radius * _radius;
+    }
+}
---
+++ Practice/Lecture.12/Lecture.12/Square.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_11
+{
+    class Square : Shapes
+    {
+        private readonly double _side;
+        public Square(int side)
+        {
+            if (side <= 0)
+            {
+                this._side = 1;
+            }
+            else if(side > 10)
+            {
+                this._side = 10;
+            }           
+            else
+                this._side = side;
+
+        }
+        public override double FigureSquare() => _side * _side;
+    }
+}
