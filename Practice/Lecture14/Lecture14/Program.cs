7b84b5a381710d8344778e7644e17a6ffea16b39
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-28 19:24:14 +0300

    first task done

---
+++ Practice/Lecture14/Lecture14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lecture14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString = "this is test string";
+            testString = testString.LeaveFirstFive();
+            Console.WriteLine(testString);
+        }
+    }
+    public static  class StringExtension
+    {
+        public static string LeaveFirstFive(this string str)
+        {
+            str = str.Substring(0, 5);
+            return str;
+        }
+        
+    }
+}
