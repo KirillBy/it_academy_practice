794fb18d8f9da1270a9c7c4526957f9785385abb
Kiryl Balanovich <balanovichks@gmail.com>
2020-01-28 19:45:01 +0300

    second task done

---
+++ Practice/Lecture14/Lecture14_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lecture14_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Song song = new Song();
+            Console.WriteLine("Enter song Name");
+            song.songName = Console.ReadLine();
+            Console.WriteLine("Enter song Lenght(min)");
+            song.songLenght = uint.Parse(Console.ReadLine());
+            Console.WriteLine("Enter song Author");
+            song.songAuthor = Console.ReadLine();
+            Console.WriteLine("Enter song Year");
+            song.songYear = uint.Parse(Console.ReadLine());
+            object obj = GetSongData(song);
+        }
+        public static object GetSongData(Song song)
+        {
+            return new { Title= song.songName, Minutes = song.songLenght, AlbumYear = song.songYear };
+        }
+    }
+}
